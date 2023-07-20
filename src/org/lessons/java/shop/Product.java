857339e117b02ea85b7c443edc089e2b9019c91f
NikolGraminova <nbackup619@gmail.com>
2023-07-20 14:17:48 +0200

    Created project workspace

---
+++ src/org/lessons/java/shop/Product.java
@@ -0,0 +1,26 @@
+package org.lessons.java.shop;
+
+/*
+Nel progetto java-oop-shop, package org.lessons.java.shop, creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- descrizione
+- prezzo
+- iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome: il codice deve avere un pad left di zeri per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Nello stesso package aggiungete una classe Main con metodo main nella quale create uno o più prodotti e testate tutte le funzionalità della classe (cioè tutti i metodi public)
+BONUS 1:  Aggiungere una classe Categoria (con nome e descrizione) e associare ad ogni Prodotto una categoria usando la composizione
+BONUS 2: Permettere di associare più di una Categoria ad ogni prodotto (usare quindi una ArrayList di categorie come tipo di attributo)
+ */
+
+
+public class Product {
+
+
+
+}
